7653654611ee38017a10ef37ba9f61531b38d7f5
Maksim Kornakov <makskornakov@ya.ru>
2022-11-27 21:43:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,24 @@ import exampleArray from './output';
 
 function App() {
   return (
-    <>
-      <div
-        style={{
-          margin: '50px auto',
-          display: 'flex',
-          outline: '1px solid black',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '600px',
-          width: '800px',
-        }}
-      >
-        <Canvas
-          width={600}
-          height={400}
-          candleArray={exampleArray.map((candle) => candle as CandleToDraw)}
-          candlesShown={100}
-        ></Canvas>
-      </div>
-    </>
+    <div
+      style={{
+        margin: '50px auto',
+        display: 'flex',
+        outline: '1px solid black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '600px',
+        width: '800px',
+      }}
+    >
+      <Canvas
+        width={600}
+        height={400}
+        candleArray={exampleArray.map((candle) => candle as CandleToDraw)}
+        candlesShown={100}
+      ></Canvas>
+    </div>
   );
 }
 
